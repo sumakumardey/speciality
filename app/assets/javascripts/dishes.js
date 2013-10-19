efa057f80ec73747c015dedd8b3aa2c85f85eff0
bsubramaniam <bsubramaniam@freshdesk.com>
2013-10-20 02:57:55 +0530

    Dish show
    Signed-off-by: bsubramaniam <bsubramaniam@freshdesk.com>

---
+++ app/assets/javascripts/dishes.js
@@ -0,0 +1,29 @@
+// # Place all the behaviors and hooks related to the matching controller here.
+// # All this logic will automatically be available in application.js.
+// # You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
+function initialize() {
+	var myLatlng = new google.maps.LatLng("<%=@dish.location.latitude%>","<%=@dish.location.longitude%>");
+	var mapOptions = {
+		zoom: 11,
+		center: myLatlng,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	}
+	var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+	var marker = new google.maps.Marker({
+		position: myLatlng,
+		map: map,
+		title: 'Hello World!'
+	});
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
+$(document).ready(function () {
+	$(".write_review").on('click', function(ev) {
+		ev.preventDefault();
+
+		$('html, body').animate({
+			scrollTop: $(".reviews_comment").offset().top
+		}, 2000);
+	});
+});
