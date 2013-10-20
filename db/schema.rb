457819148793cbe9180962688bb4c65b3c77c13b
Gautham Shankar <gautham@freshdesk.com>
2013-10-20 10:53:27 +0530

    Profile picture for user

--- db/schema.rb
+++ db/schema.rb
@@ -19,8 +19,8 @@ ActiveRecord::Schema.define(:version => 20131019013848028) do
     t.datetime "created_at",                                :null => false
     t.datetime "updated_at",                                :null => false
     t.binary   "avatar_file",         :limit => 2147483647
-    t.binary   "avatar_small_file",   :limit => 2147483647
     t.binary   "avatar_thumb_file",   :limit => 2147483647
+    t.binary   "avatar_small_file",   :limit => 2147483647
     t.string   "avatar_file_name"
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
@@ -134,6 +134,7 @@ ActiveRecord::Schema.define(:version => 20131019013848028) do
     t.string   "provider"
     t.string   "uid"
     t.integer  "attachment_id"
+    t.string   "profile_image"
   end
 
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
