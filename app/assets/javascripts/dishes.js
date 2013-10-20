5b7b77d71fcf92bc979b97a0af5a169667a590b7
sumakumardey <bappi.5476@gmail.com>
2013-10-20 18:53:03 +0530

    maps fix

--- app/assets/javascripts/dishes.js
+++ app/assets/javascripts/dishes.js
@@ -1,23 +1,7 @@
 // # Place all the behaviors and hooks related to the matching controller here.
 // # All this logic will automatically be available in application.js.
 // # You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
-function initialize() {
-	var myLatlng = new google.maps.LatLng("<%=@dish.location.latitude%>","<%=@dish.location.longitude%>");
-	var mapOptions = {
-		zoom: 11,
-		center: myLatlng,
-		mapTypeId: google.maps.MapTypeId.ROADMAP
-	}
-	var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
-	var marker = new google.maps.Marker({
-		position: myLatlng,
-		map: map,
-		title: 'Hello World!'
-	});
-}
-
-// google.maps.event.addDomListener(window, 'load', initialize);
 $(document).ready(function () {
 	$(".write_review").on('click', function(ev) {
 		ev.preventDefault();
@@ -27,3 +11,21 @@ $(document).ready(function () {
 		}, 2000);
 	});
 });
+function showLocationOnMap(latitude,longitude) {
+	var myLatlng = new google.maps.LatLng(latitude,longitude) ;
+	var mapOptions = {
+		zoom: 14,
+		center: myLatlng,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	}
+	console.log("inside intialize")
+	var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+	console.log("inside intializer map-canvas")
+	var marker = new google.maps.Marker({
+		position: myLatlng,
+		map: map,
+		title: 'Hello World!'
+	});
+	console.log("here");
+	console.log(myLatlng);
+}
